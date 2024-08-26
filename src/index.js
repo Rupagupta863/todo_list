da021479ad281dcb3f31fa94da242b7261d71800
rupagupta863 <you@example.com>
2024-08-26 11:52:55 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
 import Practice from './Practice';
 import './practice.css';
 
@@ -9,7 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    {/* <App /> */}
     <Practice />
   </>
 );
